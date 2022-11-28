788b8d2e5ed955981ddec55612e9c62d3093a061
Josué Herrera <54568984+josh-hrr@users.noreply.github.com>
2022-11-28 04:55:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+      System.out.println(fib(1)); 
+  }   
+    public static int fib(int n) {
+      int[] result = new int[100];
+        if(result[n] != 0){
+            return result[n];
+        }      
+        if(n==1){
+            return n;
+        }       
+        result[n] = fib(n-1) + fib(n-2);      
+        return result[n];
+    }
+}
