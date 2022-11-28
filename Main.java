a8a3427db4606b5a6c9a34d09e12f42a45530c79
Josué Herrera <54568984+josh-hrr@users.noreply.github.com>
2022-11-28 04:59:19 +0000

    updating class

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 class Main {
   public static void main(String[] args) {
-      System.out.println(fib(1)); 
+      System.out.println(fib(10)); 
   }   
     public static int fib(int n) {
       int[] result = new int[100];
         if(result[n] != 0){
             return result[n];
-        }      
-        if(n==1){
+        }       
+        if(n<=1){
             return n;
         }       
         result[n] = fib(n-1) + fib(n-2);      
